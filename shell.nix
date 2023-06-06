e5cf2d320ea25d589945743b9a29678274559594
Mark Petruska <petruska.mark@gmail.com>
2023-06-06 20:21:15 +0200

    Add business card 01

--- shell.nix
+++ shell.nix
@@ -1,6 +1,8 @@
 let pkgs = import <nixpkgs> {};
-
 in pkgs.mkShell {
   name        = "dev-shell";
-  buildInputs = with pkgs; [ nodejs ];
+  buildInputs = with pkgs; [
+    nodejs
+    texlive.combined.scheme-full
+  ];
 }
